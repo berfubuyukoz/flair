730653470da13cc29d24943d57e4b6a7809c92c0
Berfu BÜYÜKÖZ <buyukozb@MEFEDUITs-MacBook-Pro-6.local>
2019-08-15 13:38:42 +0300

    Fix bug on save model during training

--- flair/trainers/trainer.py
+++ flair/trainers/trainer.py
@@ -203,7 +203,8 @@ class ModelTrainer:
                             log.info(f'Weights extracted.')
                     if batch_no > 0 and save_model_period != -1 and batch_no % save_model_period == 0:
                         log.info(f'saving model on batch {batch_no}')
-                        self.model.save(base_path / 'saved-model_batch_' + str(batch_no) + '.py')
+                        save_model_name = 'saved-model_batch_' + str(batch_no) + '.py'
+                        self.model.save(base_path / save_model_name)
                 train_loss /= len(train_data)
 
                 self.model.eval()
