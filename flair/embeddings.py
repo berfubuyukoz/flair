01121840cd681c73a2ff5123dd542172c3e6345b
Berfu BÜYÜKÖZ <berfu.buyukoz@boun.edu.tr>
2019-08-28 18:23:31 +0300

    Modifications for MuseCrosslingualEmbeddings class to enable selection of a single language for the whole data.

--- flair/embeddings.py
+++ flair/embeddings.py
@@ -559,64 +559,61 @@ class BPEmbSerializable(BPEmb):
 
 
 class MuseCrosslingualEmbeddings(TokenEmbeddings):
-    def __init__(self,):
+    def __init__(self,
+                 language_code: str = None,
+                 ):
+        """
+        Initializes fasttext word embeddings. Constructor downloads required embedding file and stores in cache
+        if use_local is False.
+
+        :param language_code: if None, language will be detected for each sentence individually.
+        If not None, a single-language for the while data will be assumed. This modification done to skip redundant calls for
+        get_language_code() for each sentence.
+        """
         self.name: str = f"muse-crosslingual"
         self.static_embeddings = True
         self.__embedding_length: int = 300
         self.language_embeddings = {}
+        self.supported = ["en","de","bg","ca","hr","cs","da","nl","et","fi","fr","el","he","hu","id","it","mk","no","pl","pt","ro","ru","sk"]
+
+        # assign language code common to the input data if that is the case.
+        if language_code is not None:
+            if language_code not in self.supported:
+                self.language_code = "en"
+            else:
+                self.language_code = language_code
+            self.load_language_model(language_code)
         super().__init__()
+
+    def load_language_model(self, language_code: str = None):
+
+        if language_code not in self.supported: language_code = "en"
+
+        log.info(f"Loading up MUSE embeddings for '{language_code}'!")
+        # download if necessary
+        webpath = "https://alan-nlp.s3.eu-central-1.amazonaws.com/resources/embeddings-muse"
+        cache_dir = Path("embeddings") / "MUSE"
+        cached_path(
+            f"{webpath}/muse.{language_code}.vec.gensim.vectors.npy",
+            cache_dir=cache_dir,
+        )
+        embeddings_file = cached_path(
+            f"{webpath}/muse.{language_code}.vec.gensim", cache_dir=cache_dir
+        )
+
+        # load the model
+        self.language_embeddings[language_code] = gensim.models.KeyedVectors.load(str(embeddings_file))
+
 
     def _add_embeddings_internal(self, sentences: List[Sentence]) -> List[Sentence]:
 
         for i, sentence in enumerate(sentences):
 
-            language_code = sentence.get_language_code()
-            print(language_code)
-            supported = [
-                "en",
-                "de",
-                "bg",
-                "ca",
-                "hr",
-                "cs",
-                "da",
-                "nl",
-                "et",
-                "fi",
-                "fr",
-                "el",
-                "he",
-                "hu",
-                "id",
-                "it",
-                "mk",
-                "no",
-                "pl",
-                "pt",
-                "ro",
-                "ru",
-                "sk",
-            ]
-            if language_code not in supported:
-                language_code = "en"
+            if self.language_code is not None:
+                language_code = sentence.get_language_code()
 
-            if language_code not in self.language_embeddings:
-                log.info(f"Loading up MUSE embeddings for '{language_code}'!")
-                # download if necessary
-                webpath = "https://alan-nlp.s3.eu-central-1.amazonaws.com/resources/embeddings-muse"
-                cache_dir = Path("embeddings") / "MUSE"
-                cached_path(
-                    f"{webpath}/muse.{language_code}.vec.gensim.vectors.npy",
-                    cache_dir=cache_dir,
-                )
-                embeddings_file = cached_path(
-                    f"{webpath}/muse.{language_code}.vec.gensim", cache_dir=cache_dir
-                )
-
-                # load the model
-                self.language_embeddings[
-                    language_code
-                ] = gensim.models.KeyedVectors.load(str(embeddings_file))
+                if language_code not in self.language_embeddings:
+                    self.load_language_model(language_code)
 
             current_embedding_model = self.language_embeddings[language_code]
 
